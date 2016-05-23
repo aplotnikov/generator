9ddf409caa700b900fb7e00de7a202f2e5e4fd9f
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-05-23 23:27:43 +0200

    Add validation for incorrect constructor arguments

--- src/main/java/org/home/generator/Generator.java
+++ src/main/java/org/home/generator/Generator.java
@@ -4,15 +4,21 @@ import javax.annotation.Nonnull;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class Generator {
     private final List<String> parameters;
     private final List<String> sings;
     private final List<String> functions;
 
     public Generator(@Nonnull List<String> parameters, @Nonnull List<String> sings) {
-        this.functions = new LinkedList<>();
+        checkArgument(!parameters.isEmpty(), "\'parameters\' argument has to be not empty");
+        checkArgument(!sings.isEmpty(), "\'signs\' argument has to be not empty");
+
         this.parameters = parameters;
         this.sings = sings;
+
+        this.functions = new LinkedList<>();
     }
 
     public void generate() {
@@ -22,20 +28,20 @@ public class Generator {
     private void generateFunctions(String originalFunction, int currentIteration, int availableIteration) {
         //  TODO Need to refactor this code
         parameters.stream()
-                  .forEach(parameter -> {
-                      sings.stream()
-                           .forEach(sing -> {
-                               if (currentIteration < availableIteration) {
-                                   generateFunctions(
-                                           originalFunction + parameter + sing,
-                                           currentIteration + 1,
-                                           availableIteration
-                                   );
-                               } else {
-                                   functions.add(originalFunction + parameter);
-                               }
-                           });
-                  });
+                  .forEach(
+                          parameter ->
+                                  sings.stream()
+                                       .forEach(sing -> {
+                                           if (currentIteration < availableIteration) {
+                                               generateFunctions(
+                                                       originalFunction + parameter + sing,
+                                                       currentIteration + 1,
+                                                       availableIteration
+                                               );
+                                           } else {
+                                               functions.add(originalFunction + parameter);
+                                           }
+                                       }));
     }
 
     @Nonnull
--- src/test/groovy/org/home/generator/GeneratorTest.groovy
+++ src/test/groovy/org/home/generator/GeneratorTest.groovy
@@ -2,10 +2,35 @@ package org.home.generator
 
 import spock.lang.Specification
 import spock.lang.Title
+import spock.lang.Unroll
 
 @Title("Validation of main functionality of generator")
 class GeneratorTest extends Specification {
-    def "generator should generate four functions with input parameters: x and y and sing '+'"() {
+    @Unroll
+    def "IllegalArgumentException should be thrown when configuration contains parameters (#containsParameters) and sings (#containsSigns)"() {
+        given: "The generator configuration with given amount of parameters and sings"
+        List parameters = Mock(List.class)
+        parameters.isEmpty() >> !containsParameters
+
+        List sings = Mock(List.class)
+        sings.isEmpty() >> !containsSigns
+
+        when: "The generator is instancing"
+        new Generator(parameters, sings)
+
+        then: "IllegalArgumentException is thrown"
+        def exception = thrown(IllegalArgumentException.class)
+
+        and: "The exception message equals to expectedExceptionMessage parameter"
+        assert exception.message == expectedExceptionMessage
+
+        where:
+        containsParameters | containsSigns | expectedExceptionMessage
+        false              | true          | '\'parameters\' argument has to be not empty'
+        true               | false         | '\'signs\' argument has to be not empty'
+    }
+
+    def "The generator should generate four functions with input parameters: x and y and sing '+'"() {
 
         given: "The generator with configuration for parameters: x and y and sing '+'"
         def generator = new Generator(
@@ -23,7 +48,7 @@ class GeneratorTest extends Specification {
         assert generator.functions() == ['x+x', 'x+y', 'y+x', 'y+y']
     }
 
-    def "generator should generate one function with input parameters: x and y and sing '-'"() {
+    def "The generator should generate one function with input parameters: x and y and sing '-'"() {
 
         given: "The generator with configuration for parameters: x and y and sing '-'"
         def generator = new Generator(
