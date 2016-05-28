cd6e33fe31386a0ecb5a53dd9eaac67ea87a1987
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-05-28 18:29:26 +0200

    Fix duplicate generation bug

--- src/main/java/org/home/generator/Generator.java
+++ src/main/java/org/home/generator/Generator.java
@@ -5,47 +5,52 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Collections.unmodifiableList;
 
 public class Generator {
     private final List<String> parameters;
     private final List<String> sings;
+    private final int generatedSingsAmount;
+
     private final List<String> functions;
 
-    public Generator(@Nonnull List<String> parameters, @Nonnull List<String> sings) {
+    public Generator(@Nonnull List<String> parameters, @Nonnull List<String> sings, int generatedSingsAmount) {
         checkArgument(!parameters.isEmpty(), "\'parameters\' argument has to be not empty");
         checkArgument(!sings.isEmpty(), "\'signs\' argument has to be not empty");
+        checkArgument(generatedSingsAmount > 0, "\'generatedSignsAmount\' argument has to be not empty");
 
         this.parameters = parameters;
         this.sings = sings;
+        this.generatedSingsAmount = generatedSingsAmount;
 
         this.functions = new LinkedList<>();
     }
 
     public void generate() {
-        generateFunctions("", 0, sings.size());
+        generateFunctions("", 0, generatedSingsAmount);
     }
 
-    private void generateFunctions(String originalFunction, int currentIteration, int availableIteration) {
+    private void generateFunctions(String originalFunction, int currentIteration, int availableIterations) {
         //  TODO Need to refactor this code
         parameters.stream()
                   .forEach(
-                          parameter ->
-                                  sings.stream()
-                                       .forEach(sing -> {
-                                           if (currentIteration < availableIteration) {
-                                               generateFunctions(
-                                                       originalFunction + parameter + sing,
-                                                       currentIteration + 1,
-                                                       availableIteration
-                                               );
-                                           } else {
-                                               functions.add(originalFunction + parameter);
-                                           }
-                                       }));
+                          parameter -> {
+                              if (currentIteration < availableIterations) {
+                                  sings.stream().forEach(
+                                          sing -> generateFunctions(
+                                                  originalFunction + parameter + sing,
+                                                  currentIteration + 1,
+                                                  availableIterations
+                                          )
+                                  );
+                              } else {
+                                  functions.add(originalFunction + parameter);
+                              }
+                          });
     }
 
     @Nonnull
     public List<String> functions() {
-        return functions;
+        return unmodifiableList(functions);
     }
 }
--- src/test/groovy/org/home/generator/GeneratorTest.groovy
+++ src/test/groovy/org/home/generator/GeneratorTest.groovy
@@ -2,40 +2,68 @@ package org.home.generator
 
 import spock.lang.Specification
 import spock.lang.Title
-import spock.lang.Unroll
 
 @Title("Validation of main functionality of generator")
 class GeneratorTest extends Specification {
-    @Unroll
-    def "IllegalArgumentException should be thrown when configuration contains parameters (#containsParameters) and sings (#containsSigns)"() {
-        given: "The generator configuration with given amount of parameters and sings"
+    def "IllegalArgumentException should be thrown when no parameters are configured"() {
+        given: "The generator configuration with empty parameters configuration"
         List parameters = Mock(List.class)
-        parameters.isEmpty() >> !containsParameters
-
-        List sings = Mock(List.class)
-        sings.isEmpty() >> !containsSigns
+        parameters.isEmpty() >> true
 
         when: "The generator is instancing"
-        new Generator(parameters, sings)
+        new Generator(parameters, [], 0)
 
         then: "IllegalArgumentException is thrown"
         def exception = thrown(IllegalArgumentException.class)
 
-        and: "The exception message equals to expectedExceptionMessage parameter"
-        assert exception.message == expectedExceptionMessage
-
-        where:
-        containsParameters | containsSigns | expectedExceptionMessage
-        false              | true          | '\'parameters\' argument has to be not empty'
-        true               | false         | '\'signs\' argument has to be not empty'
+        and: "The exception message should notify about a problem with configuration with parameters"
+        assert exception.message == '\'parameters\' argument has to be not empty'
     }
+
+    def "IllegalArgumentException should be thrown when some parameters are configured and no signs are configured"() {
+        given: "The generator configuration with no empty parameters configuration and empty sings configuration"
+        List parameters = Mock(List.class)
+        parameters.isEmpty() >> false
+
+        List sings = Mock(List.class)
+        sings.isEmpty() >> true
+
+        when: "The generator is instancing"
+        new Generator(parameters, sings, 0)
+
+        then: "IllegalArgumentException is thrown"
+        def exception = thrown(IllegalArgumentException.class)
+
+        and: "The exception message should notify about a problem with configuration with signs"
+        assert exception.message == '\'signs\' argument has to be not empty'
+    }
+
+    def "IllegalArgumentException should be thrown when some parameters and signs are configured and generated signs amount param is invalid"() {
+        given: "The generator configuration with given amount of parameters and sings"
+        List parameters = Mock(List.class)
+        parameters.isEmpty() >> false
+
+        List sings = Mock(List.class)
+        sings.isEmpty() >> false
+
+        when: "The generator is instancing"
+        new Generator(parameters, sings, 0)
+
+        then: "IllegalArgumentException is thrown"
+        def exception = thrown(IllegalArgumentException.class)
+
+        and: "The exception message should notify about a problem with configuration with signs"
+        assert exception.message == '\'generatedSignsAmount\' argument has to be not empty'
+    }
+
 
     def "The generator should generate four functions with input parameters: x and y and sing '+'"() {
 
         given: "The generator with configuration for parameters: x and y and sing '+'"
         def generator = new Generator(
                 ['x', 'y'],
-                ['+']
+                ['+'],
+                1
         )
 
         when: "The generation process is launched"
@@ -48,12 +76,13 @@ class GeneratorTest extends Specification {
         assert generator.functions() == ['x+x', 'x+y', 'y+x', 'y+y']
     }
 
-    def "The generator should generate one function with input parameters: x and y and sing '-'"() {
+    def "The generator should generate four functions with input parameters: x and y and sing '-'"() {
 
         given: "The generator with configuration for parameters: x and y and sing '-'"
         def generator = new Generator(
                 ['x', 'y'],
-                ['-']
+                ['-'],
+                1
         )
 
         when: "The generation process is launched"
@@ -64,5 +93,38 @@ class GeneratorTest extends Specification {
 
         and: "The generator should generate the following list of functions: 'x-x', 'x-y', 'y-x', 'y-y'"
         assert generator.functions() == ['x-x', 'x-y', 'y-x', 'y-y']
+    }
+
+    def "The generator should generate 32 functions with input parameters: x, y and sings: '+', '-'"() {
+        given: "The generator with configuration for parameters: x, y and sings: '+', '-'"
+        def generator = new Generator(
+                ['x', 'y'],
+                ['+', '-'],
+                2
+        )
+
+        when: "The generation process is launched"
+        generator.generate()
+
+        then: "Four functions should be generated"
+        assert generator.functions().size() == 32
+    }
+
+    def "The generator should generate 8 functions with input parameter x and sings: '+', '-'"() {
+        given: "The generator with configuration for parameter x and sings: '+', '-'"
+        def generator = new Generator(
+                ['x'],
+                ['+', '-'],
+                2
+        )
+
+        when: "The generation process is launched"
+        generator.generate()
+
+        then: "Four functions should be generated"
+        assert generator.functions().size() == 4
+
+        and: "The generator should generate the following list of functions: 'x+x+x', 'x+x-x', 'x-x+x', 'x-x-x'"
+        assert generator.functions() == ['x+x+x', 'x+x-x', 'x-x+x', 'x-x-x']
     }
 }
