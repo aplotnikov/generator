b7f7fd001ce4bccad4e0397cbdc3f4d2f3b3477c
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-05-28 21:37:21 +0200

    Create Parameter class

---
+++ src/main/java/org/home/generator/function/Parameter.java
@@ -0,0 +1,52 @@
+package org.home.generator.function;
+
+import javax.annotation.Nonnull;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Parameter {
+    private final String name;
+    private final BigDecimal value;
+
+    public Parameter(@Nonnull String name, @Nonnull BigDecimal value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Nonnull
+    public String getName() {
+        return name;
+    }
+
+    @Nonnull
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object anotherObject) {
+        if (this == anotherObject) {
+            return true;
+        }
+
+        if (anotherObject == null || getClass() != anotherObject.getClass()) {
+            return false;
+        }
+
+        Parameter anotherParameter = (Parameter) anotherObject;
+        return Objects.equals(name, anotherParameter.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Parameter[" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                ']';
+    }
+}
---
+++ src/test/groovy/org/home/generator/function/ParameterTest.groovy
@@ -0,0 +1,49 @@
+package org.home.generator.function
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static java.math.BigDecimal.TEN
+
+class ParameterTest extends Specification {
+    def parameter
+
+    void setup() {
+        parameter = new Parameter('x', TEN)
+    }
+
+    def "The parameter should contain information provided via constructor"() {
+        when: "parameter is created with name='x' and value='10'"
+        then:
+        assert parameter.getName() == 'x'
+
+        and:
+        assert parameter.getValue() == TEN
+    }
+
+    @Unroll
+    def "The parameter's method equal and hashcode should be equaled(#expectedResult) when another parameter name is '#name' and value is #value"() {
+        when: "parameter is created with name='x' and value='10'"
+        and:
+        def anotherParameter = new Parameter(name, BigDecimal.valueOf(value))
+
+        then:
+        assert parameter.equals(anotherParameter) == expectedResult
+
+        and:
+        assert parameter.hashCode().equals(anotherParameter.hashCode()) == expectedResult
+
+        where:
+        name || value || expectedResult
+        'x'  || 10    || true
+        'x'  || 11    || true
+        'y'  || 10    || false
+
+    }
+
+    def "'toString' method should return information about parameter name and value"() {
+        when: "parameter is created with name='x' and value='10'"
+        then:
+        assert parameter.toString() == "Parameter[name='x', value=10]"
+    }
+}
